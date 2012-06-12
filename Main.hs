4f4f66b2249fd8cfc7d553cd6060ec9f66c40300
Joel <barebonesgraphics@gmail.com>
2012-06-11 22:42:00 -0400

    version 0.1

--- Main.hs
+++ Main.hs
@@ -1,4 +1,33 @@
 import Kevin
+import System.Console.GetOpt
+import System.Environment
+
+defaultPort :: Int
+defaultPort = 6669
+
+data Flag = Port Int | Version | Help deriving (Eq, Show)
+
+opts :: [OptDescr Flag]
+opts = [
+        Option "p" ["port"] (ReqArg (\x -> Port (read x :: Int)) "number") "local port to run the server on (defaults to 6667)",
+        Option "h" ["help"] (NoArg Help) "print this message",
+        Option "v" ["version"] (NoArg Version) "show kevin's version number"
+       ]
+
+header :: String
+header = "Usage: kevin [options...]"
+
+getPort :: [Flag] -> Int
+getPort (Port x:_) = x
+getPort (_:xs) = getPort xs
+getPort [] = defaultPort
 
 main :: IO ()
-main = kevinServer
+main = do
+    args <- getArgs
+    case getOpt Permute opts args of
+        (flags, _, []) -> case flags of
+            f | Help `elem` f -> putStrLn $ usageInfo header opts
+              | Version `elem` f -> putStrLn $ "kevin version " ++ VERSION
+              | otherwise -> kevinServer $ getPort flags
+        (_, _, msgs@(_:_)) -> putStrLn $ concat msgs ++ usageInfo header opts
