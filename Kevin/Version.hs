56d0d743a12c5f8e238faade43040de2efac217b
Joel <barebonesgraphics@gmail.com>
2013-01-27 15:56:53 -0500

    use Data.Version rather than CPP since it messes up multiline strings

---
+++ Kevin/Version.hs
@@ -0,0 +1,10 @@
+module Kevin.Version (showVersion, version, versionStr) where
+
+import Data.Text (Text, pack)
+import Data.Version
+
+version :: Version
+version = Version [0,7,1] []
+
+versionStr :: Text
+versionStr = pack $ showVersion version
